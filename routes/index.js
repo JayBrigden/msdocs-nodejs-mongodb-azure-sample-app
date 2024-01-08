867ae761477ba9c0f21f954709417f438540ea0d
JayBrigden <155751270+JayBrigden@users.noreply.github.com>
2024-01-08 16:37:48 +0000

    reverted index.js

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,13 @@
 var express = require('express');
 var Patient = require('../models/Patient');
-var Appointment = require('../models/Appointment');
-var Billing = require('../models/Billing');
-var Inventory = require('../models/Inventory');
 
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  Promise.all([Patient.find(),Appointment.find(),Billing.find(),Inventory.find()])
-    .then((collections) => {
-      const PatientRecords = collections[0]
-      const AppointmentRecords = collections[1]
-      const BillingRecords = collections[2]
-      const InventoryRecords = collections[3]
+  Promise.all(Patient.find())
+    .then((patients) => {
 
-      res.render('index', { PatientRecords: PatientRecords, AppointmentRecords: AppointmentRecords, BillingRecords: BillingRecords, InventoryRecords: InventoryRecords });
+      res.render('index', { PatientRecords: patients });
     })
     .catch((err) => {
       console.log(err);
