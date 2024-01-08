19e89b2d576890ef6fb4de2f6732071327e7e5b6
JayBrigden <155751270+JayBrigden@users.noreply.github.com>
2024-01-08 22:50:13 +0000

    typo fix

--- public/scripts/inventorys.js
+++ public/scripts/inventorys.js
@@ -2,8 +2,8 @@ var inventorySelectBox = document.getElementById('inventorySelectBox');
 
 inventorySelectBox.addEventListener('change', function(event) {
     var currentInventoryRecord = JSON.parse(inventorySelectBox.options[inventorySelectBox.selectedIndex].dataset.record);
-    document.getElementById('inventoryIdInput').value = currentInventoryRecord.inventoryID;
-    document.getElementById('inventoryNameInput').value = currentInventoryRecord.PatientName;
+    document.getElementById('inventoryIdInput').value = currentInventoryRecord.InventoryID;
+    document.getElementById('inventoryNameInput').value = currentInventoryRecord.InventoryName;
     document.getElementById('inventoryQuantityInput').value = currentInventoryRecord.Quantity;
     document.getElementById('inventorySupplierNameInput').value = currentInventoryRecord.SupplierName;
     document.getElementById('inventorySupplierContactInput').value = currentInventoryRecord.SupplierContact;
