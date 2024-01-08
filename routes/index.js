c6bb84473887e586b0fab2629926b84a070a7f5c
JayBrigden <155751270+JayBrigden@users.noreply.github.com>
2024-01-08 17:49:46 +0000

    bug fix

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var Patient = require('../models/Patient');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.send('hello world');
+  res.render('index');
 });
 
 router.get('/patients', function(req, res, next) {
