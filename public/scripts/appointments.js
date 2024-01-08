bb16e1590a6ab51e20c6d507cc27d3b561125349
JayBrigden <155751270+JayBrigden@users.noreply.github.com>
2024-01-08 18:10:07 +0000

    added appointments page

---
+++ public/scripts/appointments.js
@@ -0,0 +1,22 @@
+var appointmentSelectBox = document.getElementById('appointmentSelectBox');
+
+appointmentSelectBox.addEventListener('change', function(event) {
+        var currentAppointmentRecord = JSON.parse(appointmentSelectBox.options[appointmentSelectBox.selectedIndex].dataset.record);
+        document.getElementById('appointmentIdInput').value = currentAppointmentRecord.AppointmentID;
+        document.getElementById('appointmentPatientIdInput').value = currentAppointmentRecord.PatientID;
+        document.getElementById('appointmentPatientNameInput').value = currentAppointmentRecord.PatientName;
+        document.getElementById('appointmentDoctorIdInput').value = currentAppointmentRecord.DoctorID;
+        document.getElementById('appointmentDateInput').value = new Date(currentAppointmentRecord.AppointmentDate);
+        document.getElementById('appointmentTimeInput').value = currentAppointmentRecord.Time;
+        document.getElementById('appointmentPurposeInput').value = currentAppointmentRecord.Purpose;
+        document.getElementById('appointmentNotesInput').value = currentAppointmentRecord.Notes;
+
+});
+
+    // Copy code above
+    // replace currentpatientrecord -- currentappointmentrecord 
+    // make the appointment & inventory button interactive
+            // make new sheet 
+            //show correct data
+    // Fix Collection naming 
+            // Billing, Inventories, Appintments
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var Patient = require('../models/Patient');
+var Appointment = require('../models/Appointment');
 
 var router = express.Router();
 
@@ -12,6 +13,18 @@ router.get('/patients', function(req, res, next) {
     .then((patients) => {
 
       res.render('patients', { PatientRecords: patients });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send('Sorry Something went wrong!');
+    });
+});
+
+router.get('/appointments', function(req, res, next) {
+  Appointment.find()
+    .then((appointments) => {
+
+      res.render('appointments', { AppointmentRecords: appointments });
     })
     .catch((err) => {
       console.log(err);
