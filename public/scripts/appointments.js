d24288a0f48014f2658b05d57eb21a60cccfc37b
JayBrigden <155751270+JayBrigden@users.noreply.github.com>
2024-01-09 01:10:10 +0000

    bug fix to appointment chart

--- public/scripts/appointments.js
+++ public/scripts/appointments.js
@@ -1,30 +1,57 @@
 var appointmentSelectBox = document.getElementById('appointmentSelectBox');
 const appointmentPurposeData = [];
+const appointmentPurposeTally = {};
 var ctx = document.getElementById("appointmentChart").getContext('2d');
 
 document.addEventListener('DOMContentLoaded', function() {
         for (var i = 0; i < appointmentSelectBox.length; i++){
                 appointmentPurposeData.push(JSON.parse(appointmentSelectBox.options[i].dataset.record).Purpose);
         }
+
+        appointmentPurposeTally = appointmentPurposeData.reduce(function(tally, value){
+                tally[value] = tally[value] ? tally[value] + 1 : 1;
+                return tally;
+        }, {});
 }, false);
 
 var chart = new Chart(ctx, {
         type: 'pie',
         data: {
-                labels: Array.from(new Set(appointmentPurposeData)),
+                labels: Object.keys(appointmentPurposeTally),
                 datasets: [{
                         label: 'Appointment Purposes',
-                        data: appointmentPurposeData,
-                        borderWidth: 1
+                        data: Object.values(appointmentPurposeTally),
+                        backgroundColor: [
+                                "#00eff3",
+                                "#ae37c2",
+                                "#bbed63",
+                                "#0042c7",
+                                "#ff980d",
+                                "#00a6ff",
+                                "#007100",
+                                "#ffabff",
+                                "#6f6900",
+                                "#7f56ac",
+                                "#00e4ff",
+                                "#7a0000",
+                                "#00dfff",
+                                "#640030",
+                                "#8ef0ff",
+                                "#411600",
+                                "#ff9293",
+                                "#3f4553",
+                                "#662245",
+                                "#95899e"
+                        ]
                 }]
         },
         options: {
                 title: {
                         display: true,
-                        text: "chart"
+                        text: "Appointment Purposes"
                 },
                 legend: {
-                        position: 'bottom'
+                        position: 'right'
                 }
         }
 });
