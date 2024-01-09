c4765a8fa6f428d5c286fa35e6c0e691573336b9
JayBrigden <155751270+JayBrigden@users.noreply.github.com>
2024-01-09 01:18:50 +0000

    bug fix

--- public/scripts/appointments.js
+++ public/scripts/appointments.js
@@ -1,6 +1,5 @@
 var appointmentSelectBox = document.getElementById('appointmentSelectBox');
 const appointmentPurposeData = [];
-const appointmentPurposeTally = {};
 var ctx = document.getElementById("appointmentChart").getContext('2d');
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -8,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 appointmentPurposeData.push(JSON.parse(appointmentSelectBox.options[i].dataset.record).Purpose);
         }
 
-        appointmentPurposeTally = appointmentPurposeData.reduce(function(tally, value){
+        const appointmentPurposeTally = appointmentPurposeData.reduce(function(tally, value){
                 tally[value] = tally[value] ? tally[value] + 1 : 1;
                 return tally;
         }, {});
